34051a42cdc4c70c9cb067e15ab2e8af0b03dd25
deshinta <deshintaaulia065@gmail.com>
2024-07-16 11:10:33 +0700

    feat: add layout login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,53 @@
 export default function Login() {
   return (
     <>
-      <div className="h-screen flex justify-center items-center">
-        <div className="p-7 md:p-10 shadow-md rounded h-3/5 md:h-1/3">
-          <h1 className="text-center mb-12 font-semibold text-2xl">Login</h1>
-          <form className="gap-4">
-            <div className="grid">
-              <label htmlFor="email">Email</label>
-              <input type="email" className="rounded p-2 border" />
-            </div>
-            <div className="grid">
-              <label htmlFor="password">Password</label>
-              <input type="password" className="rounded p-2 border" />
-            </div>
-          </form>
+      <div className="w-screen h-screen bg-cover flex bg-[#9bbefe]">
+        <div className="bg-white rounded-lg w-11/12 h-11/12 m-5">
+          <div className="p-5">
+            <img src="/blue-waves.png" alt="logo" className="w-1/4 m-3" />
+            <h1 className="text-5xl font-bold">Masuk Ke Platform iProc</h1>
+            <p className="text-slate-600 text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              enim.
+            </p>
+            <form action="" className="mt-4 p-7">
+              <div className="text-2xl font-semibold grid">
+                <label className="text-slate-600" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="rounded p-2 border mt-2 focus:outline-none focus:ring-1 ring-sky-400 invalid:text-pink-700 invalid:ring-pink-700 text-xl"
+                />
+              </div>
+              <div className="text-2xl font-semibold grid mt-6">
+                <label className="text-slate-600" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="rounded p-2 border mt-2 focus:outline-none focus:ring-1 ring-sky-400 invalid:text-pink-700 invalid:ring-pink-700 text-xl"
+                />
+              </div>
+              <button
+                className="py-2 px-4 bg-sky-800 text-xl rounded font-semibold text-white mt-11"
+                type="submit"
+              >
+                Login to your account
+              </button>
+              <p className="text-xl mt-5">
+                Don't have an account ?
+                <span className="hover:text-sky-700 hover:underline cursor-pointer">
+                  Sign Up
+                </span>
+              </p>
+            </form>
+          </div>
         </div>
+        <div
+          className="w-screen h-screen bg-cover bg-center p-10"
+          style={{ backgroundImage: "url('/login.jpg')" }}
+        ></div>
       </div>
     </>
   );
